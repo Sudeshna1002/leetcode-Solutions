3b152043c39506dee0943753094bd7399bcdf8c6
Sudeshna1002 <kondurusudeshna@gmail.com>
2024-06-30 22:32:49 +0530

    Time: 54 ms (30.33%), Space: 16.5 MB (34.33%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -2,7 +2,7 @@ class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         from collections import Counter
         result=[]
-        counts=Counter(sorted(nums)).most_common()[::-1]
-        for i,j in counts:
+        c=Counter(sorted(nums)).most_common()[::-1]
+        for i,j in c:
             result.extend(i for k in range(j))
         return result
