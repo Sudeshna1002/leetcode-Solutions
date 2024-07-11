eebaec14dcdb730dc1decdd64502b09cedab87a4
Sudeshna1002 <kondurusudeshna@gmail.com>
2024-07-11 10:00:35 +0530

    Time: 39 ms (56.65%), Space: 16.6 MB (27.42%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        arr=[]
+        for i in range(1,n+1):
+            arr.append(i)
+        index=0
+        while len(arr)!=1:
+            
+                index=index+k-1
+           
+                while index>=len(arr):  
+                  index=index-len(arr)
+                arr.pop(index)
+        return arr[0]
+                
+            
+            
