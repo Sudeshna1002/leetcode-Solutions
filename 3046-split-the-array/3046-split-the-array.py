1ce0da331353ca25338093d40e44fc16962e1cf2
Sudeshna1002 <kondurusudeshna@gmail.com>
2024-07-12 19:18:23 +0530

    Time: 57 ms (5.89%), Space: 16.5 MB (28.68%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.py
+++ 3046-split-the-array/3046-split-the-array.py
@@ -1,23 +1,8 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
         from collections import Counter
-        nums1=[]
-        nums2=[]
+        
         if max(Counter(nums).values())>2:
             return False
-           
-        for num in nums:
-            if len(nums1)==len(nums)/2:
-                nums2.append(num)
-            elif len(nums2)==len(nums)/2:
-                nums1.append(num)
-            else:    
-                if num not in nums1:
-                    nums1.append(num)
-         
-                elif num not in nums2:
-                    nums2.append(num)
-               
-                else:
-                    return False
-        return len(nums1) == len(nums2) == len(nums) // 2
+        else:
+            return True
