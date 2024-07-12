5932a3537ec16fc3410259885187fee754b9b17b
Sudeshna1002 <kondurusudeshna@gmail.com>
2024-07-12 18:40:47 +0530

    Time: 48 ms (51.47%), Space: 16.4 MB (70.94%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        for i in nums:
+            if nums.count(i)>2:
+                return False
+        return True  
